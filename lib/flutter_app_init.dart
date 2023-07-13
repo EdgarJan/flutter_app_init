ced12e1a472936e536b121c02ea7be039725c3b4
Edgar Jan <EdgarJan@users.noreply.github.com>
2023-07-13 17:09:08 +0300

    delete realm

--- lib/flutter_app_init.dart
+++ lib/flutter_app_init.dart
@@ -1,54 +1,133 @@
-library flutter_app_init;
-
+//Maybe it needs dispose method
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app_init/mongo_data_wrapper/mongo_data_wrapper.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:realm/realm.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-export 'package:easy_localization/easy_localization.dart';
-// carry on sentry ignore
-// ignore: invalid_export_of_internal_element
-export 'package:sentry_flutter/sentry_flutter.dart';
-export 'package:loader_overlay/loader_overlay.dart';
 
-appInit(
-    {String? sentryDsn,
-    required Widget body,
-    required List<SchemaObject> schemaObjects,
-    required void Function(
-            MutableSubscriptionSet mutableSubscriptions, Realm realm)
-        subscriptionCallback,
-    required String realmAppId,
-    List<Locale>? supportedLocales}) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (supportedLocales != null){
-    await EasyLocalization.ensureInitialized();
+class MongoDataWrapper extends InheritedWidget {
+  final String _appId;
+  late final AppConfiguration _appConfig;
+  late final App _app;
+  final ValueNotifier<Realm?> realm = ValueNotifier<Realm?>(null);
+  final List<SchemaObject> schemaObjects;
+  final void Function(MutableSubscriptionSet mutableSubscriptions, Realm realm)
+      subscriptionCallback;
+
+  MongoDataWrapper(
+      {Key? key,
+      required String appId,
+      required Widget child,
+      required this.schemaObjects,
+      required this.subscriptionCallback,
+      List<Locale>? supportedLocales})
+      : _appId = appId,
+        super(
+            key: key,
+            child: supportedLocales != null
+                ? EasyLocalization(
+                    supportedLocales: supportedLocales,
+                    path: 'assets/translations',
+                    fallbackLocale: supportedLocales.first,
+                    child: Builder(builder: (context) {
+                      return MaterialApp(
+                        theme: ThemeData(
+                          useMaterial3: true,
+                        ),
+                        localizationsDelegates: context.localizationDelegates,
+                        supportedLocales: context.supportedLocales,
+                        locale: context.locale,
+                        home: LoaderOverlay(
+                          useDefaultLoading: false,
+                          overlayWidget: const Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                          child: child,
+                        ),
+                      );
+                    }),
+                  )
+                : MaterialApp(
+                    theme: ThemeData(
+                      useMaterial3: true,
+                    ),
+                    home: LoaderOverlay(
+                      useDefaultLoading: false,
+                      overlayWidget: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                      child: child,
+                    ),
+                  )) {
+    _appConfig = AppConfiguration(_appId);
+    _app = App(_appConfig);
+    _initRealm();
   }
-  if (sentryDsn != null) {
-    await SentryFlutter.init(
-      (options) {
-        options.dsn = sentryDsn;
-        options.tracesSampleRate = 1.0;
-      },
-      appRunner: () => runApp(
-        MongoDataWrapper(
-          appId: realmAppId,
-          schemaObjects: schemaObjects,
-          subscriptionCallback: subscriptionCallback,
-          supportedLocales: supportedLocales,
-          child: body,
-        ),
-      ),
-    );
-  } else {
-    runApp(
-      MongoDataWrapper(
-        appId: realmAppId,
-        schemaObjects: schemaObjects,
-        subscriptionCallback: subscriptionCallback,
-        supportedLocales: supportedLocales,
-        child: body,
-      ),
-    );
+
+  @override
+  bool updateShouldNotify(covariant MongoDataWrapper oldWidget) {
+    return oldWidget._appId != _appId || realm != oldWidget.realm;
+  }
+
+  static MongoDataWrapper? maybeOf(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<MongoDataWrapper>();
+  }
+
+  dynamic customData() {
+    return _app.currentUser?.customData;
+  }
+
+  logOut({required BuildContext context}) {
+    context.loaderOverlay.show();
+    try {
+      _app.currentUser?.logOut().then((value) {
+        final tempRealm = realm.value;
+        realm.value = null;
+        tempRealm?.close();
+        context.loaderOverlay.hide();
+      });
+    } catch (e) {
+      context.loaderOverlay.hide();
+    }
+  }
+
+  logIn(
+      {required Credentials credentials, required BuildContext context}) async {
+    context.loaderOverlay.show();
+    _app.logIn(credentials).then((value) {
+      _initRealm();
+      context.loaderOverlay.hide();
+    });
+  }
+
+  _initRealm() {
+    if (_app.currentUser != null) {
+      _app.currentUser!.refreshCustomData();
+      final configuration =
+          Configuration.flexibleSync(_app.currentUser!, schemaObjects,
+              syncErrorHandler: (SyncError error) {
+        if (kDebugMode) {
+          print("Error message${error.message}");
+        }
+        Sentry.captureException(
+          error,
+        );
+      });
+      Realm? tempRealm;
+      try {
+        tempRealm = Realm(configuration);
+      } catch (e) {
+        tempRealm?.close();
+        _app.currentUser?.logOut();
+        Realm.deleteRealm(configuration.path);
+        _initRealm();
+        return;
+      }
+      tempRealm.subscriptions.update((mutableSubscriptions) {
+        subscriptionCallback.call(mutableSubscriptions, tempRealm!);
+      });
+      realm.value = tempRealm;
+    }
   }
 }
